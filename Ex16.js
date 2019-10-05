466451fb7ca72fb7bbe3dfbdc8d2ceb66d35c009
spaceCampBoy <260101@via.dk>
2019-10-05 12:16:05 +0200

    final

--- Ex16.js
+++ Ex16.js
@@ -1,6 +1,14 @@
+var body = document.body;
+    body.innerHTML = '<table id="weatherData"><tbody>' + 
+    '<tr><th>place</th>' + 
+    '<th>precipitation_type</th>' +
+    '<th>time</th>' +
+    '<th>type</th>' +
+    '<th>unit</th>' +
+    '<th>value</th></tr></tbody></table>'
+
 const XMLHttpRequest = this.XMLHttpRequest
-console.log("hello")
-function getHorsensWeatherData(callback)
+function getHorsensWeatherDataXHR(callback)
 {
     const request = new XMLHttpRequest();
     request.addEventListener('load', () => {
@@ -11,29 +19,57 @@ function getHorsensWeatherData(callback)
     request.send()
 }
 
+function getHorsensWeatherDataFetch(callback)
+{
+    let promise = fetch(`http://localhost:8080/data/Horsens`).then(res => res.ok ? res.json() : Promise.reject(res.statusText))
+    promise.then(res => callback(res))
+}
+
 function displayData(data)
 {
-    var body = document.body;
-    body.innerHTML = '<table id="weatherData"><tbody>' + 
-    '<tr><th>place</th>' + 
-    '<th>precipitation_type</th>' +
-    '<th>time</th>' +
-    '<th>type</th>' +
-    '<th>unit</th>' +
-    '<th>value</th></tr>'
+    console.log(data)
+
+    var tableRef = document.getElementById('weatherData').getElementsByTagName('tbody')[0];
 
     for (let index = 0; index < data.length; index++) {
         const  e = data[index];
-        body.innerHTML += 
-            '<tr><td>' + e.place + ' </td>' + 
-            '<td>' + e.precipitation_type + ' </td>' +
-            '<td>' + e.time + ' </td>' +
-            '<td>' + e.type + ' </td>' +
-            '<td>' + e.unit + ' </td>' +
-            '<td>' + e.value + ' </td></tr>';
+        // Insert a row in the table at the last row
+        var newRow   = tableRef.insertRow();
+
+        var newCell  = newRow.insertCell(0);
+        var newText  = document.createTextNode(e.place);
+        newCell.appendChild(newText);
+
+        newCell  = newRow.insertCell(1);
+        newText  = document.createTextNode(e.precipitation_type);
+        newCell.appendChild(newText);
+
+        newCell  = newRow.insertCell(2);
+        newText  = document.createTextNode(e.time);
+        newCell.appendChild(newText);
+
+        newCell  = newRow.insertCell(3);
+        newText  = document.createTextNode(e.type);
+        newCell.appendChild(newText);
+
+        newCell  = newRow.insertCell(4);
+        newText  = document.createTextNode(e.unit);
+        newCell.appendChild(newText);
+
+        newCell  = newRow.insertCell(5);
+        newText  = document.createTextNode(e.value);
+        newCell.appendChild(newText);
+
+        // body.innerHTML += 
+        //     '<tr><td>' + e.place + ' </td>' + 
+        //     '<td>' + e.precipitation_type + ' </td>' +
+        //     '<td>' + e.time + ' </td>' +
+        //     '<td>' + e.type + ' </td>' +
+        //     '<td>' + e.unit + ' </td>' +
+        //     '<td>' + e.value + ' </td></tr>';
     }
-    body.innerHTML += '</tbody></table>'
 }
 
-getHorsensWeatherData(displayData)
+getHorsensWeatherDataXHR(displayData)
+getHorsensWeatherDataFetch(displayData)
 
